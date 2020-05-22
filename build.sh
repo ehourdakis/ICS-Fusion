e51113671d54319c77707216cff248b2261df949
virtual <nik.tavu@gmail.com>
2020-05-22 18:10:04 +0300

    download isam and TooN

--- build.sh
+++ build.sh
@@ -5,15 +5,30 @@ SCRIPT=$(readlink -f $0)
 SCRIPT_PATH=`dirname $SCRIPT`
 OLD_PATH=`pwd`
 
-#==============
-sudo apt-get install -y subversion
-#==============
+#==============Install depentencies through apt===============
+sudo apt-get install -y subversion libsuitesparse-dev
+#=============================================================
 echo $SCRIPT_PATH $CATKIN_WS
-mkdir $SCRIPT_PATH/dependencies 2>/dev/null 
-
+mkdir -p $SCRIPT_PATH/dependencies 
 cd $SCRIPT_PATH/dependencies
-svn co https://svn.csail.mit.edu/isam
-exit
+
+
+#=========================ISAM==========================
+git clone https://github.com/ori-drs/isam.git
+cd isam
+git apply $SCRIPT_PATH/isam_pic.patch
+mkdir -p build 
+cd build
+cmake ..
+#=======================================================
+
+#=========================TOON==========================
+cd $SCRIPT_PATH/dependencies
+git clone https://github.com/edrosten/TooN.git
+cd $SCRIPT_PATH/dependencies/TooN
+./configure && make
+#=======================================================
+
 
 ln -s $SCRIPT_PATH/ROS $CATKIN_WS/src/ics_fusion
 
